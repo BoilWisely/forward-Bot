b76f26a3e6f08474405e66ed55983c97996ad500
BoilWisely <149a00@gmail.com>
2025-02-01 00:34:47 +0600

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "22205516"))
     API_HASH = environ.get("API_HASH", "2b0b60e7a0404c343c5c02365b8b3f42")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6040428516") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://149a00:Hy2aP2niTlifaFKo@cluster0.b7xlb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6040428516"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
